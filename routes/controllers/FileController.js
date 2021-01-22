61ea47a21bdd65014d127e6de47e744c559442e2
Demian <devaccdemiann@gmail.com>
2021-01-22 21:12:13 +0100

    Refactored and added methods for index and show

--- routes/controllers/FileController.js
+++ routes/controllers/FileController.js
@@ -2,8 +2,9 @@ const slug = require("slug");
 const shortid = require("shortid");
 
 const AWS = require("aws-sdk");
-const DB = require("@db");
+const path = require("path");
 
+const DB = require("@db");
 const { DOMAIN, AWS_BUCKET, AWS_ENDPOINT, AWS_KEY, AWS_SECRET } = process.env;
 
 const space = new AWS.S3({
@@ -13,9 +14,20 @@ const space = new AWS.S3({
   credentials: new AWS.Credentials(AWS_KEY, AWS_SECRET, null),
 });
 
-exports.store = async (req, res, next) => {
-  const slugy = slug(`${req.file.originalname} ${shortid.generate()}`);
+exports.index = async (req, res, next) => {
+  space.listObjects({ Bucket: AWS_BUCKET }, async (error, data) => {
+    if (error) {
+      console.error(error);
+      return res.sendStatus(500);
+    }
+    const files = await DB.File.find({});
+    res.json({ files, data });
+  });
+};
 
+exports.store = async (req, res, next) => {
+  let slugy = slug(`${req.file.originalname} ${shortid.generate()}`);
+  slugy += path.extname(req.file.originalname);
   const uploadParameters = {
     Bucket: AWS_BUCKET,
     ContentType: req.file.mimetype,
@@ -30,13 +42,24 @@ exports.store = async (req, res, next) => {
       return res.sendStatus(500);
     }
 
-    const image = new DB.Image({
+    const File = new DB.File({
       slugy,
       longCDN: data.Location,
       shortCDN: `${DOMAIN}${data.Key}`,
+      type: path.extname(req.file.originalname),
     });
-    await image.save();
+    await File.save();
 
     res.sendStatus(200);
+  });
+};
+
+exports.show = async (req, res, next) => {
+  space.getObject({ Bucket: AWS_BUCKET, Key: req.params.slug }, (error, data) => {
+    if (error) {
+      console.error(error);
+      return res.sendStatus(500);
+    }
+    res.json({ data });
   });
 };
