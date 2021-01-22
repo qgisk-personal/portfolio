c529feeca0f5161e45be6b09dc7acc52606ab01a
Demian <devaccdemiann@gmail.com>
2021-01-22 20:30:21 +0100

    has perm and ensureAuth

--- routes/middleware/auth.js
+++ routes/middleware/auth.js
@@ -1,1 +1,37 @@
 const DB = require("@db");
+
+exports.ensureAuthenticated = async (req, res, next) => {
+  try {
+    let token =
+      req.headers["x-access-token"] ||
+      req.headers["authorization"] ||
+      req.query.bearer ||
+      req.query.authorization;
+    if (token) {
+      if (token.startsWith("Bearer ")) {
+        token = token.slice(7, token.length);
+      }
+      jwt.verify(token, JWT_SECRET, async (err, decoded) => {
+        if (err) {
+          return res.send("Page not found").status("404");
+        }
+        const user = await DB.User.findById(decoded.id);
+
+        if (!user) return res.send("Page not found").status("404");
+
+        req.user = user;
+        next();
+      });
+    } else {
+      return res.send("Page not found").status("404");
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+exports.hasPerm = perm => {
+  return function (req, res, next) {
+    if (!req.user.permissions[perm]) return res.send("Page not found").status("404");
+    else next();
+  };
+};
