2757b24c15f54224aaf31357b69969f1f8402926
Demian <devaccdemiann@gmail.com>
2021-01-22 19:57:35 +0100

    Add uploader

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const { Contact, Limiter } = require("./helpers/Limiters");
 const Controller = require("./controllers/ContactController");
+const imageController = require("./controllers/ImageController");
+const { upload } = require("./helpers/ImageFilter");
 
 router.post("/contact", Contact, Controller.store);
+router.route("/image", Limiter).post(upload.single("image"), imageController.store);
 
 module.exports = router;
