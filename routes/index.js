6077695cb9788fc9177504c99e591f2ad4ca99e6
Demian <devaccdemiann@gmail.com>
2021-01-22 21:12:34 +0100

    Refactored and added get and show

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { Contact, Limiter } = require("./helpers/Limiters");
 const Controller = require("@controllers/ContactController");
 const FileController = require("@controllers/FileController");
 const AuthController = require("@controllers/AuthController");
+
 const { upload } = require("@helpers/fileFilter");
+const { Contact, Limiter } = require("@helpers/Limiters");
 const { ensureAuthenticated, hasPerm } = require("@middleware/auth");
 
 router.post("/contact", Contact, Controller.store);
@@ -14,6 +15,9 @@ router.post("/auth/login", Contact, AuthController.login);
 
 router
   .route("/file", Limiter)
+  .get(ensureAuthenticated, hasPerm("ACCESS"), FileController.index)
   .post(ensureAuthenticated, hasPerm("WRITE"), upload.single("file"), FileController.store);
+
+router.route("/file/:slug").get(ensureAuthenticated, hasPerm("ACCESS"), FileController.show);
 
 module.exports = router;
