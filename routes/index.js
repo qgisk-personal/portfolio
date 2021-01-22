98837fef0d4a2fb542cd3cbb478faf2b8a255893
Demian <devaccdemiann@gmail.com>
2021-01-22 20:30:08 +0100

    Add middleware

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const { Contact, Limiter } = require("./helpers/Limiters");
-const Controller = require("./controllers/ContactController");
-const fileController = require("./controllers/FileController");
-const { upload } = require("./helpers/fileFilter");
+const Controller = require("@controllers/ContactController");
+const fileController = require("@controllers/FileController");
+const { upload } = require("@helpers/fileFilter");
+const { ensureAuthenticated, hasPerm } = require("@middleware/auth");
 
 router.post("/contact", Contact, Controller.store);
-router.route("/file", Limiter).post(upload.single("file"), fileController.store);
+
+router
+  .route("/file", Limiter)
+  .post(ensureAuthenticated, hasPerm("admin"), upload.single("file"), fileController.store);
 
 module.exports = router;
